e489a1164112802dbbd160b321c1ee4c39745e37
joezayas <joedza4610@gmail.com>
2022-12-21 14:43:56 -0600

    Features added successfully

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         CarFeatures Car = new CarFeatures();
         System.out.println("launch the car");
 
+        Car.color="red";
+        Car.doors="4";
+        Car.engine=5;
+        Car.model="Hellcat";
+
     }
 }
